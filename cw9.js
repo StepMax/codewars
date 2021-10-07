7d9ad1409f31a0de83ce5bd023d5c8cce1016557
Max Stepanovich <maxstepanovich@MacBook-Pro-Max.local>
2021-10-07 23:32:24 +0300

    Sums

---
+++ cw9.js
@@ -0,0 +1,9 @@
+function f(n){
+    let sum=0;
+      if (Number.isInteger(n) && n>0){
+     for(let i=1; i<=n; i++) {
+    
+      sum+=i
+    }return sum} else return false
+    //insert your code here.... and go crazy!
+    };
