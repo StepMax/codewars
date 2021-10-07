8927427a64f88a69baae93a5100ae55c4f1b9c91
Max Stepanovich <maxstepanovich@MacBook-Pro-Max.local>
2021-10-07 23:34:34 +0300

    perfect square

---
+++ cw11.js
@@ -0,0 +1,10 @@
+function findNextSquare(sq) {
+    return Number.isInteger(Math.sqrt(sq)) ? Math.pow((Math.sqrt(sq)) + 1, 2): - 1;
+   
+   
+   
+       
+     // Return the next square if sq is a perfect square, -1 otherwise
+   
+   }
+   
